408e826a16c92265494a3afca6fcad2df6bb92f4
cudytechnologies <dev@cudy.co>
2023-04-11 19:41:00 +0500

    Main tasks

---
+++ src/scripts/helpers/countdown.ts
@@ -0,0 +1,26 @@
+export default class CountDown extends Phaser.Events.EventEmitter {
+  private timedEvent: Phaser.Time.TimerEvent
+  private scene: Phaser.Scene
+  private current: number
+  private delay = 1000
+
+  constructor(scene: Phaser.Scene) {
+    super()
+    this.scene = scene
+  }
+
+  start(time = 0) {
+    this.current = time
+    this.timedEvent?.remove()
+    this.timedEvent = this.scene.time.addEvent({
+      delay: this.delay,
+      callback: () => {
+        this.current++
+        this.emit('interval', this.current)
+      },
+      loop: true,
+    })
+
+    return this
+  }
+}
